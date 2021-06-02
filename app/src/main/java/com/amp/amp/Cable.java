9b01458f03620eed8fb1d8fc7d890ea696f505df
anykey196 <anykey196@gmail.com>
2021-06-02 11:02:40 +0500

    add

--- app/src/main/java/com/amp/amp/Cable.java
+++ app/src/main/java/com/amp/amp/Cable.java
@@ -4,15 +4,15 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
-public class Cable {
+public class type_of_environment {
     @PrimaryKey
-    public int id;
-    public String [] insulation_type;
-    public String [] material_type;
-    public String [] nominal_size;
-    public String [] number_of_cores;
-    public String [] short_current;
-    public String [] type_amperage;
     public String [] type_of_environment;
-    public String [] sposob_prokladki;
+  /*  public String [] insulation_type = {"PVC", "Polyetilen"};
+    public String [] material_type = {"Cu", "Al"};
+    public String [] nominal_size;
+    public String [] number_of_cores = {"Mono", "Mnogoprovoloca))"};
+    public String [] short_current;
+    public String [] type_amperage = {"AC", "DC"};
+    public String [] type_of_environment = {"Air", "Earth"};
+    public String [] sposob_prokladki = {"Odinokaya", "V puchke))"};*/
 }
